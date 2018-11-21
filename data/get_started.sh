65effcf3ebeb47a3b6f32ed6381460c4bf2f4bd1
Komal Kumar <tesseract28@Komals-MacBook-Pro.local>
2018-11-21 02:40:42 -0500

    Updatetd getting started script

--- data/get_started.sh
+++ data/get_started.sh
@@ -1,11 +1,15 @@
 #!usr/bin/env bash
 
+echo 'Downloading the UTKFaces dataset.....'
 ./gdown.pl https://drive.google.com/file/d/0BxYys69jI14kYVM3aVhKS1VhRUk/view UTKFace.tar.gz
 
+echo 'Extracting the UTKFaces dataset.....'
 tar -xzf UTKFace.tar.gz
 
+echo 'Correcting mislabelled datapoints...'
 mv ./UTKFace/39_1_20170116174525125.jpg.chip.jpg ./UTKFace/39_1_1_20170116174525125.jpg.chip.jpg
 mv ./UTKFace/61_1_20170109150557335.jpg.chip.jpg ./UTKFace/61_1_3_20170109150557335.jpg.chip.jpg
 mv ./UTKFace/61_1_20170109142408075.jpg.chip.jpg ./UTKFace/61_1_1_20170109142408075.jpg.chip.jpg
 
-python data-analysis.py
+echo 'Starting pre-processing....'
+python data_analysis.py
